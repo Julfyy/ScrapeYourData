b119d3f2fadd35d5d9592601e891691aafa3b2bc
Bohdan <bohdan7565@gmail.com>
2021-03-22 22:06:43 +0200

    set up linter and formatter

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 /* global chrome */
-document.addEventListener('DOMContentLoaded', () => {
-  const clickButton = document.getElementById('click')
-  clickButton.addEventListener('click', function () {
-    chrome.tabs.getCurrent(function (tab) {
-      window.open('https://www.google.com/')
-    })
-  }, false)
-}, false)
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const clickButton = document.getElementById('click');
+    clickButton.addEventListener(
+      'click',
+      function () {
+        chrome.tabs.getCurrent(function (tab) {
+          window.open('https://www.google.com/');
+        });
+      },
+      false
+    );
+  },
+  false
+);
