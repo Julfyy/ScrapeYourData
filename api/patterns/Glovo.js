b5dfdd536bc4ad10c19eae8417a96e780e10972e
Julfyy <bohdan7565@gmail.com>
2021-05-20 09:09:39 +0300

    formatted

--- api/patterns/Glovo.js
+++ api/patterns/Glovo.js
@@ -1,64 +1,68 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
 const Excel = require('exceljs');
 
 const getData = async (url) => {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(url);
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(url);
 
-    const data = await page.evaluate(() => {
-        const titles = document.querySelectorAll('h4.title');
-        const descriptions = document.querySelectorAll('div.description span span');
-        const prices = document.querySelectorAll('span.product-price__effective');
-        const arr = [];
+  const data = await page.evaluate(() => {
+    const titles = document.querySelectorAll('h4.title');
+    const descriptions = document.querySelectorAll('div.description span span');
+    const prices = document.querySelectorAll('span.product-price__effective');
+    const arr = [];
 
-        // const regexPrice = /[0-9]*\,|\.?[0-9]*/
+    // const regexPrice = /[0-9]*\,?\.?[0-9]*/
 
-        for (let i = 0; i < titles.length; i++) {
-            arr.push(
-                {
-                    title: titles[i] === undefined ? "" : titles[i].innerHTML,
-                    description: descriptions[i] === undefined ? "" : descriptions[i].innerHTML,
-                    price: prices[i] === undefined ? "" : prices[i].innerHTML //.match(regexPrice)
-                }
-            )
-        }
+    for (let i = 0; i < titles.length; i++) {
+      arr.push({
+        title: titles[i] === undefined ? '' : titles[i].innerHTML,
+        description:
+          descriptions[i] === undefined ? '' : descriptions[i].innerHTML,
+        price: prices[i] === undefined ? '' : prices[i].innerHTML, //.match(regexPrice)
+      });
+    }
 
-        return arr;
-    })
+    return arr;
+  });
 
-    console.log(data);
-    return data;
-}
+  console.log(data);
+  return data;
+};
 
 module.exports = async (url) => {
-    const data = await getData(url);
+  const data = await getData(url);
 
-    var workbook = new Excel.Workbook();
+  var workbook = new Excel.Workbook();
 
-    workbook.creator = 'Me';
-    workbook.lastModifiedBy = 'Her';
-    workbook.created = new Date();
-    workbook.modified = new Date();
-    workbook.lastPrinted = new Date();
-    workbook.properties.date1904 = true;
+  workbook.creator = 'Me';
+  workbook.lastModifiedBy = 'Her';
+  workbook.created = new Date();
+  workbook.modified = new Date();
+  workbook.lastPrinted = new Date();
+  workbook.properties.date1904 = true;
 
-    workbook.views = [
-        {
-            x: 0, y: 0, width: 10000, height: 20000,
-            firstSheet: 0, activeTab: 1, visibility: 'visible'
-        }
-    ];
-    var worksheet = workbook.addWorksheet('Glovo');
-    worksheet.columns = [
-        { header: 'Title', key: 'title', width: 30 },
-        { header: 'Description', key: 'description', width: 100 },
-        { header: 'Price', key: 'price', width: 10 }
-    ];
+  workbook.views = [
+    {
+      x: 0,
+      y: 0,
+      width: 10000,
+      height: 20000,
+      firstSheet: 0,
+      activeTab: 1,
+      visibility: 'visible',
+    },
+  ];
+  var worksheet = workbook.addWorksheet('Glovo');
+  worksheet.columns = [
+    { header: 'Title', key: 'title', width: 30 },
+    { header: 'Description', key: 'description', width: 100 },
+    { header: 'Price', key: 'price', width: 10 },
+  ];
 
-    data.forEach(value => {
-        worksheet.addRow(value);
-    })
+  data.forEach((value) => {
+    worksheet.addRow(value);
+  });
 
-    return workbook;
-}
+  return workbook;
+};
--- api/router.js
+++ api/router.js
@@ -3,47 +3,46 @@ const router = express.Router();
 
 const Glovo = require('./patterns/Glovo.js');
 
-const urls = [
-    'https://glovoapp.com/'
-];
+const urls = ['https://glovoapp.com/'];
 
 const userUrls = new Map();
-  
+
 router.get('/', (_, res) => {
-    res.send('Server works');
+  res.send('Server works');
 });
 
 router.post('/isAvailable', (req, res) => {
-    const url = req.body.url;
-    const requestId = new Date().getTime();
-    
-    for (const availableUrl of urls) {
-        if (url.includes(availableUrl)) {
-            res.status(200).json({
-                isAvailable: true,
-                id: requestId
-            });
+  const url = req.body.url;
+  const requestId = new Date().getTime();
 
-            userUrls.set(requestId, url);
-            return;
-        }
+  for (const availableUrl of urls) {
+    if (url.includes(availableUrl)) {
+      res.status(200).json({
+        isAvailable: true,
+        id: requestId,
+      });
+
+      userUrls.set(requestId, url);
+      return;
     }
+  }
 
-    res.status(200).json({ isAvailable: false });
+  res.status(200).json({ isAvailable: false });
 });
 
 router.get('/scrape/xlsx/:id', (req, res) => {
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    res.setHeader("Content-Disposition", "attachment; filename=" + "Data.xlsx");
+  res.setHeader(
+    'Content-Type',
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+  );
+  res.setHeader('Content-Disposition', 'attachment; filename=' + 'Data.xlsx');
 
-    const key = Number(req.params.id)
-    Glovo(userUrls.get(key))
-        .then(file => {
-            file.xlsx.write(res)
-                .then(function (_) {
-                    res.end();
-                });
-        })
+  const key = Number(req.params.id);
+  Glovo(userUrls.get(key)).then((file) => {
+    file.xlsx.write(res).then(function () {
+      res.end();
+    });
+  });
 });
 
 module.exports = router;
